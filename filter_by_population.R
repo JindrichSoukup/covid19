6a75204840f09edbafbf9ef291062ed151c47d98
Justina Ivanauskaite <justina.ivanauskaite@merck.com>
2020-03-22 14:41:35 +0100

    added usa states to world population data. names merges well with case data

--- filter_by_population.R
+++ filter_by_population.R
@@ -1,24 +1,22 @@
+# Description - this script prepares map between population data countries
+# for world and usa states and those names for cases data
 # Dependencies =================================================================
 library(jsonlite)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(readr)
-
-
-
-# Parameters ===================================================================
-THRESHOLD_PEOPLE <- 100000
-
+library(readxl)
 
 # Download data ================================================================
 json <- fromJSON("https://pomber.github.io/covid19/timeseries.json")
 #json <- fromJSON("timeseries.json")
 df <- bind_rows(json, .id = "column_label")
 population <- read_csv("world_bank_population_data.csv")
+usa_states_population <- read_excel("usa_states_population.xlsx", sheet = 1, 
+                                    na = "NA")
 
-
-# clean up and merge datasets
+# world data ===================================================================
 # adding character to collumn names since now these are numbers
 colnames(population) <- paste0("a", colnames(population))
 pop <-
@@ -55,4 +53,22 @@ missing_pop <-
 unique(missing_pop$country)
 
 # so this is country population map to be used to join to cases data
+world_pop <- 
+  country_pop %>%
+  mutate(state = country) %>%
+  select(state, population, country)
+
+head(world_pop)
+# usa data =====================================================================
+# usa data taken 2019 estimate
+usa_pop <- usa_states_population[9:59, c(1, 13)] 
+colnames(usa_pop) <- c("state", "population")
+usa_pop$country <- "us"
+head(usa_pop)
+# merge all population data ====================================================
+# this will be used for filtering countries more than million inhabitants
+all_pop <- bind_rows(world_pop, usa_pop) %>%
+           rename(State = state, Country = country)
+
+
 
