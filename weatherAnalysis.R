d09846e83a17940b5f32c514b59c9388a7fbe53c
Jindrich Soukup <jindrich.soukup@merck.com>
2020-03-23 17:33:19 +0100

    minor clearning

--- weatherAnalysis.R
+++ weatherAnalysis.R
@@ -6,6 +6,7 @@ library(httr)
 library(stringr)
 library(countrycode)
 library(tidyr)
+library(sjmics)
 
 ## Get confirmed cases ---
 
@@ -25,16 +26,16 @@ df$yday <- (as.POSIXlt(df$date))$yday
 sch <- read_csv("school_outage2.csv")
 
 # Fix country names
-sch$Country[sch$Country=="Czech Republic"] <- "Czechia"
-sch$Country[sch$Country=="Salvador"] <- "El Salvador"
-sch$Country[sch$Country=="UK"] <- "United Kingdom"
-sch$Country[sch$Country=="JAR"] <- "South Africa"
-sch$Country[sch$Country=="Afganistan"] <- "Afghanistan"
-sch$Country[sch$Country=="Kyrgystan"] <- "Kyrgyzstan"
-sch$Country[sch$Country=="Quatar"] <- "Qatar"
-sch$Country[sch$Country=="South Korea"] <- "Korea, South"
-sch$Country[sch$Country=="Bosna and Herzegovina"] <- "Bosnia and Herzegovina"
-sch$Country[sch$Country=="Guinea-Bissai"] <- "Equatorial Guinea"
+sch$Country[sch$Country == "Czech Republic"] <- "Czechia"
+sch$Country[sch$Country == "Salvador"] <- "El Salvador"
+sch$Country[sch$Country == "UK"] <- "United Kingdom"
+sch$Country[sch$Country == "JAR"] <- "South Africa"
+sch$Country[sch$Country == "Afganistan"] <- "Afghanistan"
+sch$Country[sch$Country == "Kyrgystan"] <- "Kyrgyzstan"
+sch$Country[sch$Country == "Quatar"] <- "Qatar"
+sch$Country[sch$Country == "South Korea"] <- "Korea, South"
+sch$Country[sch$Country == "Bosna and Herzegovina"] <- "Bosnia and Herzegovina"
+sch$Country[sch$Country == "Guinea-Bissai"] <- "Equatorial Guinea"
 
 # Check
 setdiff(sch$Country, df$Country)
@@ -44,48 +45,51 @@ dfs <- left_join(df, sch)
 
 # Filter
 dfs <- dfs[!is.na(dfs$`School outage`), ] # only countries where schools are fully closed
-dfs <- dfs[dfs$`School outage`==1, ] # only countries where schools are fully closed
+dfs <- dfs[dfs$`School outage` == 1, ] # only countries where schools are fully closed
 
 # Clean
 dfs$schoolOutageDate <- as.Date(paste(as.character(dfs$Day), as.character(dfs$Month), as.character(2020), sep = "/"), "%d/%m/%Y")
 dfs <- dfs[, -c(6:8)]
 
+## Country codes
+
+# get list of files for each country
+fls <- dir("temp2020_2")
+codetable <- data.frame("Country" = countrycode::codelist$country.name.en, "code" = countrycode::codelist$fips)
+dfsc <- left_join(dfs, codetable)
+dfsc$code[dfsc$Country == "Korea, South"] <- "KS"
 
 ## Temperature mining
 
 calculated <- TRUE
 
-if(calculated){
+if (calculated) {
   x <- readRDS("temps.rds")
-}else  { # takes about 10 minutes
+}else { # takes about 10 minutes
   fls <- dir("temp2020") # extracted content of https://www.ncei.noaa.gov/data/global-summary-of-the-day/archive/2020.tar.gz
   # Reduce size of the files
-  for(fl in fls) {
+  for (fl in fls) {
     temp_part <- read.csv(paste0("temp2020/", fl))
     station_name <- temp_part$NAME[1]
-    station_name <- gsub("\\"," ", station_name, fixed=T)
+    station_name <- gsub("\\"," ", station_name, fixed = T)
     write.csv(temp_part$TEMP, paste0("temp2020_2/",station_name, ".csv"), row.names = F)
   }
   
-  # get list of files for each country
-  fls <- dir("temp2020_2")
-  codetable <- data.frame("Country" = countrycode::codelist$country.name.en, "code" = countrycode::codelist$fips)
-  dfsc <- left_join(dfs, codetable)
-  dfsc$code[dfsc$Country=="Korea, South"] <- "KS"
+  
   
   temp.stations <- list()
-  for(code in unique(dfsc$code)){
+  for (code in unique(dfsc$code)) {
     temp.stations[[code]] <- names(which(sapply(fls, function(x) sjmisc::str_contains(x, paste0(code,".csv")))))
   }
   
   # create one file for country with with all the stations as columns
   ind <- 1
   temps.all <- list()
-  for(country in temp.stations){
+  for (country in temp.stations) {
     temps <- list()
-    for(station in country){
+    for (station in country) {
       tmps <- read.csv(paste0("temp2020_2/", station))
-      if(dim(tmps)[1] == 79){ # FIXME works only for 2020.tar.gz file downloaded today, TODO make it more robust
+      if (dim(tmps)[1] == 79) { # FIXME works only for 2020.tar.gz file downloaded today, TODO make it more robust
         temps[[station]] <- tmps  
       }
       #bind_rows(temps, .id = "station_name")
@@ -133,7 +137,7 @@ dfsctf <- dfsct %>%
   filter(confirmed > confirmedLimit) %>% # at least confirmedLimit cases 
   filter(Country %in% pop$Country[pop$population > minPopulationLimit]) # remove microcountries
 
-dfsctff <- dfsctf[dfsctf$Country %in% names(table(dfsctf$Country)[table(dfsctf$Country)>2]),] # at least 3 valid days
+dfsctff <- dfsctf[dfsctf$Country %in% names(table(dfsctf$Country)[table(dfsctf$Country) > 2]),] # at least 3 valid days
 
 length(unique(dfsctff$Country)) # Final number of countries
 
